af5d10c262e6e1ec9c68fb1903df344b26789757
Java-Instructor <143517387+Java-instructor@users.noreply.github.com>
2023-08-30 16:47:46 +0530

    loaded default data for test

--- src/main/java/com/java/instructor/spring/microservice/departments/DepartmentMain.java
+++ src/main/java/com/java/instructor/spring/microservice/departments/DepartmentMain.java
@@ -3,6 +3,11 @@ package com.java.instructor.spring.microservice.departments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import javax.sql.DataSource;
 
 @SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
 public class DepartmentMain {
@@ -11,4 +16,10 @@ public class DepartmentMain {
 		SpringApplication.run(DepartmentMain.class, args);
 	}
 
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.H2).addScript("loadDefaultData.sql").build();
+		return db;
+	}
 }
